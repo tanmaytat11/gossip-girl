262c39010663ce449a7966c7f9b31d3dcc690152
Tanmay Gupta <tanmay.tat11@gmail.com>
2017-03-16 02:27:13 +0530

    comment the collections used

--- imports/api/gossips.js
+++ imports/api/gossips.js
@@ -1,5 +1,7 @@
 import { Mongo } from 'meteor/mongo';
- 
+
+// Collection to store gossips
 export const Gossips = new Mongo.Collection('gossips');
 
+// Collection to store the list of users a user has subscribed to following one to many relationship
 export const Subs = new Mongo.Collection('Subs');
